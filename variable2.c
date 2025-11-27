2443ba501f8b2eb525da59cb4e4c886078538a2a
jhnvi-T <jahnavireddy0106@gmail.com>
2025-11-27 13:13:16 +0530

    c program for variable intialization

---
+++ variable2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int age = 20;
+float weight = 5.7;
+char name = 'A';
+printf("Age: %d\n",age);
+printf("Height: %.1f\n",weight);
+printf("Grade: %c\n",name);
+  return 0;
+}
