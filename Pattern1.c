68fcfa8035060be64c7e546a2cefd61ebb5a46ea
jhnvi-T <jahnavireddy0106@gmail.com>
2025-12-11 23:35:28 +0530

    Update Pattern1.c

--- Pattern1.c
+++ Pattern1.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int n=5;
-for(int i=1;i<n;i++)
-{
-      for(int j=0;j<n-i;j++)
-       {
-          printf("");
-        }
-       for(int k=0;k<1*i;k++)
+    int rows=5;
+    for(int i=1;i<rows;i++)
+    {
+        for(int j=0;j<=rows-i;j++)
         {
-         printf("*");
-         }
-printf("/n");
-}
-return 0;
+            printf(" ");
+        }
+        for(int k=0;k<1*i;k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
 }
