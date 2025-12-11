09353739cc48c680081a647a9a04892cef8d09bd
jhnvi-T <jahnavireddy0106@gmail.com>
2025-12-11 23:26:00 +0530

    Add if-else ladder program for age eligibility

---
+++ if-else ladder.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i=20;
+if(i==10)
+{
+printf("not eligible for vote\n");
+}
+else if(i==15)
+{
+printf("wait for three years\n");
+}
+else if(i==20)
+{
+printf("not a valid age\n");
+}
+return 0;
+}
