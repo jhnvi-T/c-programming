888ba47929b70e3fba474ddb4283a7d59f554b05
jhnvi-T <jahnavireddy0106@gmail.com>
2025-12-27 00:24:17 +0530

    to demonstrate variable swapping with out using third variable

---
+++ Top4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int x = 10;
+int y = 20;
+printf("x: %d , y: %d\n", x, y);
+    // x hold 30
+    x = x + y;
+    // y hold 10
+    y = x - y;
+    // Now, x hold 20
+    x = x - y;
+printf("x: %d , y: %d\n", x, y);
+return 0;
+}
